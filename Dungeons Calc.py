887283627c81afff48e565a8427a6107d34c6d82
JSOProductions <71542525+JSOProductions@users.noreply.github.com>
2023-10-25 10:12:23 +1100

    d
    d

---
+++ Dungeons Calc.py
@@ -0,0 +1,25 @@
+# modules
+
+
+
+# variables #
+
+# Runes #
+Rune1 = "C"
+Rune2 = "R"
+Rune3 = "I"
+Rune4 = "A"
+Rune5 = "Q"
+Rune6 = "U"
+Rune7 = "S"
+Rune8 = "T"
+Rune9 = "P"
+
+
+
+# functions
+
+
+
+# code
+
